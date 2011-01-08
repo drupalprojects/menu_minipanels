b502f5ee47ca56a458b41e1abc19940223141083
Willie <willieseabrook@373883.no-reply.drupal.org>
2011-01-08 09:37:41 +0000

    #945886 - Using the same minipanel for more than one menu item doesnt work. This is fixed with some re-engineering to use the mlid in the drupal.settings array and panel id. This fix will break existing sites until the menu items are edited and resaved. The time required to write an automated upgrade path is not justified when a few manual clicks will do

--- js/menu_minipanels.js
+++ js/menu_minipanels.js
@@ -27,9 +27,9 @@ Drupal.behaviors.menuMiniPanels = function(context) {
 
   // Add the hovers to each appropriate menu item.
   $('ul li a.menu-minipanel').each(function() {
-    var matches = $(this).attr('class').match('menu-minipanel-([0-9a-zA-Z\_]+)');
-    var html = $('div.' + matches[1]).clone().show();
-    var settings = Drupal.settings.menuMinipanels.panels[matches[1]];
+	var matches = $(this).parent().attr('class').match('menu-([0-9]+)');
+    var html = $('div.menu-minipanel-' + matches[1]).clone().show();
+    var settings = Drupal.settings.menuMinipanels.panels['panel_' + matches[1]];
     settings['hide']['fixed'] = true;
     settings['content'] = html;
     $(this).qtip(settings);
