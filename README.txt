6de6153d5f7d1794cfb6004f53ea7e4acbf8a4e2
Damien McKenna <damien@mc-kenna.com>
2011-06-12 18:08:45 -0400

    Issue #1012044 by DamienMcKenna, uk: Initial port to Drupal 7.

--- README.txt
+++ README.txt
@@ -1,20 +1,27 @@
+Menu MiniPanels
 
-Allows an administrator to specify a minipanel to be associated with a Drupal menu item.
-When that menu item is hovered or clicked (as per config), the minipanel content will be shown
-using the qTip javascript library
+Allows an administrator to specify a minipanel to be associated with a Drupal
+menu item.  When that menu item is hovered or clicked (as per config), the
+minipanel content will be shown using the qTip javascript library
 
 Installation
 ------------
 
-1. Add the module to your modules directory
-2. Visit http://craigsworks.com/projects/qtip/download/ and download the *minified* qTip version. 
-   The name of the file you need from the archive is jquery.qtip-1.0.0-rc3.min.js.
-3. Place this file in the js/lib subfolder
-4. Enable the module
-5. Visit "Menu Minipanels Settings" to confgure default settings for qTip
+1. Add the module to your modules directory.
+2. Visit http://craigsworks.com/projects/qtip/download/ and download the
+   *minified* qTip version. The name of the file you need from the archive is
+   jquery.qtip-1.0.0-rc3.min.js
+3. Place this file in the sites/all/libraries/qtip directory of your Drupal
+   installation so that the file is available at:
+   sites/all/libraries/qtip/jquery.qtip-1.0.0-rc3.min.js
+4. Enable the module.
+5. Visit "Menu Minipanels Settings" to configure default settings for qTip.
 
 Author
 ------
 
 Willie Seabrook
 willie@themesandmodules.com
+
+Drupal 7 port by uk (http://drupal.org/user/1100194) and DamienMcKenna
+(http://drupal.org/user/108450).
--- js/menu_minipanels.js
+++ js/menu_minipanels.js
@@ -1,49 +1,50 @@
+/**
+ * @file
+ * Custom JS for the Menu_MiniPanels module.
+ */
 
 /**
- * In admin menu edit, this hides and closes the qTip config
- * depending on whether a minipanel is selected.
+ * In admin menu edit, this hides and closes the qTip config depending on
+ * whether a minipanel is selected.
  * 
- * On every page, it adds minipanel hovers to any menu items that have minipanel
- * qTip config arrays specified in the page footer
+ * On every page, it adds minipanel hovers to any menu items that have
+ * minipanel qTip config arrays specified in the page footer.
  */
-Drupal.behaviors.menuMiniPanels = function(context) {
-  // In the administration section, hide hover settings unless a minipanel is selected
-  var toggleHoverSettings = function() {
-    if($('#edit-menu-options-minipanel').val() == '' && $('#menu-minipanels-hover-settings').is(':visible')) {
-      $('#menu-minipanels-hover-settings').slideUp(500);
-    }
-    else if($('#edit-menu-options-minipanel').val() != '') {
-      $('#menu-minipanels-hover-settings').slideDown(500);
-    }
-  }
-  
-  $('#edit-menu-options-minipanel').change(function(e) {
-    toggleHoverSettings();
-  });
-  
-  // Set appropriate on load
-  toggleHoverSettings();
+(function($) {
+  Drupal.behaviors.menuMiniPanels = {
+    attach: function(context, settings) {
+      // Site admin.
 
-  // Add the hovers to each appropriate menu item.
-  $('ul li a.menu-minipanel').each(function() {
-	var matches = $(this).attr('class').match('menu-minipanel-([0-9]+)');
-    var html = $('div.menu-minipanel-' + matches[1]).clone().show();
-    var settings = Drupal.settings.menuMinipanels.panels['panel_' + matches[1]];
-    settings['hide']['fixed'] = true;
-    // Specify a custom target.
-    if (settings['position']['target'] == 'custom') {
-      var target = $(settings['position']['target_custom']);
-      if (target.length > 0) {
-        settings['position']['target'] = target;
+      // In the administration section, hide hover settings unless a minipanel
+      // is selected.
+      var toggleHoverSettings = function() {
+        if ($('#edit-menu-options-minipanel').val() == '' && $('#menu-minipanels-hover-settings').is(':visible')) {
+          $('#menu-minipanels-hover-settings').slideUp(500);
+        }
+        else if ($('#edit-menu-options-minipanel').val() != '') {
+          $('#menu-minipanels-hover-settings').slideDown(500);
+        }
       }
-      else {
-        settings['position']['target'] = false; 
-      }
+        
+      $('#edit-menu-options-minipanel').change(function(e) {
+        toggleHoverSettings();
+      });
+        
+      // Set appropriate on load
+      toggleHoverSettings();
+
+
+      // Normal use.
+
+      // Add the hovers to each appropriate menu item.
+      $('ul#main-menu-links li a').each(function() {
+        var matches = $(this).attr('class').match('menu-minipanel-([a-zA-Z0-9\_]+)');
+        var html = $('div.' + matches[1]).clone().show();
+        var panel_settings = settings.menuMinipanels.panels[matches[1]];
+        panel_settings['hide']['fixed'] = true;
+        panel_settings['content'] = html;
+        $(this).qtip(panel_settings);
+      });
     }
-    else {
-      settings['position']['target'] = false; 
-    }
-    settings['content'] = html;
-    $(this).qtip(settings);
-  });
-};
+  };
+})(jQuery);
