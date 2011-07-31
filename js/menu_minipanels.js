06ed8c7a1a2cce7f3a3c108fe842d68b701b01de
Damien McKenna <damien@mc-kenna.com>
2011-07-30 23:22:09 -0400

    Issue #1199412 by DamienMcKenna: Separate the admin JS into its own file.

--- js/menu_minipanels.js
+++ js/menu_minipanels.js
@@ -4,31 +4,10 @@
  */
 
 /**
- * In admin menu edit, this hides and closes the qTip config depending on
- * whether a minipanel is selected.
- * 
- * On every page, it adds minipanel hovers to any menu items that have
- * minipanel qTip config arrays specified in the page footer
+ * Add minipanel hovers to any menu items that have minipanel qTip config
+ * arrays specified in the page footer.
  */
 Drupal.behaviors.menuMiniPanels = function(context) {
-  // In the administration section, hide hover settings unless a minipanel is
-  // selected.
-  var toggleHoverSettings = function() {
-    if($('#edit-menu-options-minipanel').val() == '' && $('#menu-minipanels-hover-settings').is(':visible')) {
-      $('#menu-minipanels-hover-settings').slideUp(500);
-    }
-    else if($('#edit-menu-options-minipanel').val() != '') {
-      $('#menu-minipanels-hover-settings').slideDown(500);
-    }
-  }
-  
-  $('#edit-menu-options-minipanel').change(function(e) {
-    toggleHoverSettings();
-  });
-  
-  // Set appropriate on load.
-  toggleHoverSettings();
-
   // Add the hovers to each appropriate menu item.
   $('ul li a.menu-minipanel').each(function() {
     var matches = $(this).attr('class').match('menu-minipanel-([0-9]+)');
---
+++ js/menu_minipanels_admin.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Custom JS for administering Menu_MiniPanels.
+ */
+
+/**
+ * In admin menu edit, this hides and closes the qTip config depending on
+ * whether a minipanel is selected.
+ */
+Drupal.behaviors.menuMiniPanels = function(context) {
+  // In the administration section, hide hover settings unless a minipanel is
+  // selected.
+  var toggleHoverSettings = function() {
+    if($('#edit-menu-options-minipanel').val() == '' && $('#menu-minipanels-hover-settings').is(':visible')) {
+      $('#menu-minipanels-hover-settings').slideUp(500);
+    }
+    else if($('#edit-menu-options-minipanel').val() != '') {
+      $('#menu-minipanels-hover-settings').slideDown(500);
+    }
+  }
+  
+  $('#edit-menu-options-minipanel').change(function(e) {
+    toggleHoverSettings();
+  });
+  
+  // Set appropriate on load.
+  toggleHoverSettings();
+};
